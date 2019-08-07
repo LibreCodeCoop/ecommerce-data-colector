ae7a1301cd754d965fd262299603149b897cb93e
Vitor Mattos <vitor@php.rio>
2019-08-06 22:37:34 -0300

    bugfix no último commit

--- src/Coleta/Db/DbTrait.php
+++ src/Coleta/Db/DbTrait.php
@@ -11,7 +11,7 @@ trait DbTrait
     {
         if (!$this->pdo) {
             $this->pdo = new \PDO(
-                'pgsql:host=f'.getenv('DB_HOST').';'.
+                'pgsql:host='.getenv('DB_HOST').';'.
                 'dbname='.getenv('DB_NAME').';'.
                 'user='.getenv('DB_USER').';'.
                 'password='.getenv('DB_PASSWD')
